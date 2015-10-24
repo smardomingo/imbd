6f73d25654d8d4434685447f7963a648bcb6aed6
smardomingo <sara@MacBook-Pro-de-Sara.local>
2015-10-24 11:58:43 +0200

    create basic structure

---
+++ imbd.rb
@@ -0,0 +1,21 @@
+class Bars
+
+end
+
+class MovieNumber
+
+end
+
+class Legend
+
+end
+
+class Movie
+	movies = IO.readlines("movies.txt")
+end
+
+class ImbdSearch
+	movies.each do |title|
+		
+	end
+end
